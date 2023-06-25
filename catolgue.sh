12ac6ad93e1d91bb25cc083b5cd3cb28221370de
Ramsai35 <mudurukollaramsai335@gmail.com>
2023-06-25 11:37:16 +0530

    frontend

---
+++ catolgue.sh
@@ -0,0 +1,44 @@
+source common.sh
+
+print_head "Downloading Node Repo"
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${LOG}
+status_check
+
+print_head "Install Nodejs"
+yum install nodejs -y &>>${LOG}
+status_check
+
+print_head "Useradd"
+useradd roboshop &>>${LOG}
+status_check
+
+mkdir /app &>>${LOG}
+
+print_head "Downloadinga and Extracting App content"
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>${LOG}
+cd /app
+unzip /tmp/catalogue.zip
+status_check
+
+cd /app
+
+print_head "Installing Dependencies"
+npm install &>>${LOG}
+status_check
+
+cp ${script_location}/files/catalogue-conf /etc/systemd/system/catalogue.service &>>${LOG}
+
+systemctl daemon-reload &>>${LOG}
+
+print_head "Starting Service"
+systemctl enable catalogue &>>${LOG}
+systemctl start catalogue &>>${LOG}
+status_check
+
+cp ${script_location}/files/mongoclient /etc/yum.repos.d/mongo.repo &>>${LOG}
+
+print_head "Installing Mongoclient"
+yum install mongodb-org-shell -y &>>${LOG}
+status_check
+
+mongo --host 172.31.84.182 </app/schema/catalogue.js &>>${LOG}
