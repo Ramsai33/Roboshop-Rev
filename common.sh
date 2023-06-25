44dbb6e67f4a418cd347365f4e5c344d1e076132
Ramsai35 <mudurukollaramsai335@gmail.com>
2023-06-25 10:52:05 +0530

    frontend

---
+++ common.sh
@@ -0,0 +1,16 @@
+script_location=$(pwd)
+LOG=/tmp/roboshop.log
+
+print_head() {
+  echo -e "\e[1m $1\e[0m"
+}
+
+status_check() {
+  if [ $? -eq 0 ]; then
+    echo -e "\e[31mSUCCESS\e[0m"
+  else
+    echo -e "\e[33mFAILURE\e[0m"
+    echo "Please Refer log for more info -${LOG}"
+  exit1
+  fi
+}
--- frontend.sh
+++ frontend.sh
@@ -1,23 +1,26 @@
-script_location=$(pwd)
-LOG=/tmp/roboshop.log
+source common.sh
 
-echo -e "\e[31m Install nginx \e[0m"
+print_head "Install Nginx"
 yum install nginx -y &>>${LOG}
-if [ $? -eq 0 ]; then
-  echo -e "\e[32mSUCCESS\e[0m"
-else
-  echo -e "\e[33mFAILURE\e[0m"
-  echo "Please Refer log for more info -${LOG}"
-exit1
-fi
+status_check
 
+print_head "enable Service"
 systemctl enable nginx &>>${LOG}
+status_check
+
+print_head "start service"
 systemctl start nginx &>>${LOG}
+status_check
 
+print_head "removing Default content"
 rm -rf /usr/share/nginx/html/* &>>${LOG}
+status_check
 
+print_head "Extracting Content"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${LOG}
+status_check
 
 cd /usr/share/nginx/html &>>${LOG}
 unzip /tmp/frontend.zip &>>${LOG}
 
+${script_location}/files/frontend-conf /etc/nginx/default.d/roboshop.conf
