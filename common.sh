c72646d7b2da56e6c8df7c78fe5e8ab4b83bad27
Ramsai35 <mudurukollaramsai335@gmail.com>
2023-06-25 10:55:26 +0530

    frontend

--- common.sh
+++ common.sh
@@ -7,7 +7,7 @@ print_head() {
 
 status_check() {
   if [ $? -eq 0 ]; then
-    echo -e "\e[31mSUCCESS\e[0m"
+    echo -e "\e[32mSUCCESS\e[0m"
   else
     echo -e "\e[33mFAILURE\e[0m"
     echo "Please Refer log for more info -${LOG}"
